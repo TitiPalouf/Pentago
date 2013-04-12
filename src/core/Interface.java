fcfe31a565d89dfdcdfc472c14236acac198d629
TitiPalouf <titi-palouf@hotmail.fr>
2013-04-12 11:03:51 +0300

    Update Interface.java

--- src/core/Interface.java
+++ src/core/Interface.java
@@ -32,7 +32,7 @@ public class Interface {
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        System.out.println("Coucou Soinou");
 
     }
 
